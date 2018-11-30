fb410eab2580056751cfe5d73acea7beed38b915
FrancisLeigh <git@francisleigh.co.uk>
2018-11-30 08:30:07 +0000

    Basket component

---
+++ src/components/Basket.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="basket">
+    <h2>{{title}}</h2>
+    <h3>Start to add basket-related components here.</h3>
+  </div>
+</template>
+
+<script>
+import basketData from '../data/basket.js'
+export default {
+  name: 'basket',
+  props: [ 'title' ],
+  data () {
+    return {
+      basket: basketData
+    }
+  }
+}
+</script>
+
+<style>
+.basket {
+  border: 2px solid red;
+}
+</style>
