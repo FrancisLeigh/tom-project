6299be44b84b746c53c1313c070c8234fd9801e4
FrancisLeigh <git@francisleigh.co.uk>
2018-11-30 08:30:15 +0000

    basket stub data

---
+++ src/data/basket.js
@@ -0,0 +1,22 @@
+const Basket = [
+  {
+    id: 1,
+    name: 'Mountain Dew',
+    quantity: 0,
+    price: 1.30
+  },
+  {
+    id: 2,
+    name: 'Desperados',
+    quantity: 2,
+    price: 2.58
+  },
+  {
+    id: 3,
+    name: 'Jack Daniels',
+    quantity: 0,
+    price: 3.35
+  }
+]
+
+export default Basket
